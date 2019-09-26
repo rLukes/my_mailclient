bfda71266c84db572a645391ef1a3851dfad0715
Ruben <lukesruben@gmail.com>
2019-09-26 13:40:59 +0200

    added font_awsome and dreawer

--- lib/message_list.dart
+++ lib/message_list.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:my_mailclient/Message.dart';
 import 'package:my_mailclient/compose_button.dart';
 import 'package:my_mailclient/message_detail.dart';
 
 class MessageList extends StatefulWidget {
   final String title;
+
   const MessageList({Key key, this.title}) : super(key: key);
 
   @override
@@ -29,6 +31,67 @@ class _MessageListState extends State<MessageList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountEmail: Text("demo@email.com"),
+              accountName: Text("User"),
+              currentAccountPicture: CircleAvatar(
+                child: Text("A"),
+              ),
+              otherAccountsPictures: <Widget>[
+                GestureDetector(
+                  child: CircleAvatar(
+                    child: Icon(Icons.add),
+                  ),
+                  onTap: (){
+                    showDialog(context: context,
+                    builder: (context){
+                      return AlertDialog(title: Text("Adding new acount...."),);
+                    });
+                  },
+                )
+              ],
+            ),
+
+            ListTile(
+              leading: Icon(FontAwesomeIcons.inbox),
+              title: Text("Inbox"),
+              trailing: Chip(
+                label: Text("11"),
+                backgroundColor: Colors.grey,
+              )
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.edit),
+              title: Text("Draft"),
+            ),
+            ListTile(
+              title: Text("Archive"),
+              leading: Icon(FontAwesomeIcons.archive),
+            ),
+            ListTile(
+              title: Text("Sent"),
+              leading: Icon(FontAwesomeIcons.paperPlane),
+            ),
+            ListTile(
+              title: Text("Trash"),
+              leading: Icon(FontAwesomeIcons.trash),
+            ),
+            Divider(),
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.bottomRight,
+                child: ListTile(
+                  title: Text("Settings"),
+                  leading: Icon(FontAwesomeIcons.cog),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
       floatingActionButton: ComposeButton(messages),
       appBar: AppBar(
         title: Text(widget.title),
