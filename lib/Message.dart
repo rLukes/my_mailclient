0148ffbc56ecddba89329211de1be83c3d754586
Ruben <lukesruben@gmail.com>
2019-09-26 11:01:53 +0200

    using futurebuilder to build the message list

--- lib/Message.dart
+++ lib/Message.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:json_annotation/json_annotation.dart';
+import 'package:http/http.dart' as http;
 
 part 'Message.g.dart';
 
@@ -12,4 +15,17 @@ class Message{
   factory Message.fromJson(Map<String, dynamic> json) =>
       _$MessageFromJson(json);
 
+
+  static Future<List<Message>> browse() async{
+    //String content = await rootBundle.loadString('data/message.json');
+    http.Response response = await http.get("http://www.mocky.io/v2/5d8b6d1c3500005c00d46fd4");
+    await Future.delayed(Duration(seconds: 1));
+
+    String content = response.body;
+
+    List collection = json.decode(content);
+
+    List<Message> _messages = collection.map((m) => new Message.fromJson(m)).toList();
+    return _messages;
+  }
 }
--- lib/message_list.dart
+++ lib/message_list.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:my_mailclient/Message.dart';
-import 'package:http/http.dart' as http;
+
 
 class MessageList extends StatefulWidget {
   final String title;
@@ -13,25 +11,11 @@ class MessageList extends StatefulWidget {
 }
 
 class _MessageListState extends State<MessageList> {
-  var messages = const [];
-
-  void loadMessageList() async{
-    //String content = await rootBundle.loadString('data/message.json');
-    http.Response response = await http.get("http://www.mocky.io/v2/5d8b6d1c3500005c00d46fd4");
-    String content = response.body;
-
-    List collection = json.decode(content);
-
-    List<Message> _messages = collection.map((m) => new Message.fromJson(m)).toList();
-
-    setState(() {
-      messages = _messages;
-    });
-  }
+  Future<List<Message>> messages;
 
   @override
   void initState() {
-    loadMessageList();
+    messages = Message.browse();
     super.initState();
   }
 
@@ -40,28 +24,50 @@ class _MessageListState extends State<MessageList> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: (){
+            var _messages = Message.browse();
+            setState(() {
+              messages = _messages;
+            });
+          },)
+        ],
       ),
-      body: ListView.separated(
-        itemCount: messages.length,
-        separatorBuilder: (context, index) {
-          return Divider();
-        },
-        itemBuilder: (BuildContext context, int index) {
-          Message message = messages[index];
-          return ListTile(
-            title: Text(message.subject),
-            subtitle: Text(
-              message.body,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(
-              child: Text("AS"),
-            ),
-            isThreeLine: true,
-          );
-        },
-      ),
+      body: FutureBuilder(
+          future: messages,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+              case ConnectionState.active:
+                return Center(child: CircularProgressIndicator());
+              case ConnectionState.done:
+                if(snapshot.hasError) return Text("Erro!!");
+
+                var messages = snapshot.data;
+                return ListView.separated(
+                  itemCount: messages.length,
+                  separatorBuilder: (context, index) {
+                    return Divider();
+                  },
+                  itemBuilder: (BuildContext context, int index) {
+                    Message message = messages[index];
+                    return ListTile(
+                      title: Text(message.subject),
+                      subtitle: Text(
+                        message.body,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      leading: CircleAvatar(
+                        child: Text("AS"),
+                      ),
+                      isThreeLine: true,
+                    );
+                  },
+                );
+            }
+          }),
     );
   }
 }
